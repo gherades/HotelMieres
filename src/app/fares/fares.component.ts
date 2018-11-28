ee901ce1da70d82f2f9a3caee77e58b0fd4f9e30
Gonzalo.Hernando <gonzalo.hernando@sngular.team>
2018-11-28 07:24:26 +0100

    srcs

--- src/app/fares/fares.component.ts
+++ src/app/fares/fares.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fares.component.scss']
 })
 export class FaresComponent implements OnInit {
-  selectedFare: string;
   constructor() { }
 
   ngOnInit() {
